daa3a4529c41cc5333c0533d5007fe732f2ddba5
moussa <ahmedmoussa.dev@gmail.com>
2024-07-06 02:31:49 +0300

    🚧 Hardcoded version

---
+++ D&DHC/hardcoded.py
@@ -0,0 +1,64 @@
+class moussa:
+  health = 10
+  gold = 0
+  weapons = []
+  items = []
+  level = 1
+  max_health = 10 + 5 * level
+  class_type = ["necromancer"]
+  
+class saif:
+  health = 10
+  gold = 0
+  weapons = []
+  items = []
+  level = 1
+  max_health = 10 + 5 * level
+  class_type = ["bandit"]
+  
+class khalid:
+  health = 10
+  gold = 0
+  weapons = []
+  items = []
+  level = 1
+  max_health = 10 + 5 * level
+  class_type = ["comedian"]
+  
+class omar:
+  health = 10
+  gold = 0
+  weapons = []
+  items = []
+  level = 1
+  max_health = 10 + 5 * level
+  class_type = ["fighter"]
+  
+class nour:
+  health = 10
+  gold = 0
+  weapons = []
+  items = []
+  level = 1
+  max_health = 10 + 5 * level
+  class_type = ["fighter"]
+
+class waleed:
+  health = 10
+  gold = 0
+  weapons = []
+  items = []
+  level = 1
+  max_health = 10 + 5 * level
+  class_type = ["fighter"]
+  
+class mohamed:
+  health = 10
+  gold = 0
+  weapons = []
+  items = []
+  level = 1
+  max_health = 10 + 5 * level
+  class_type = ["fighter"]
+  
+print(moussa.weapons)
